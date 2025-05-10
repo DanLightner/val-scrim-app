ffb435bcf070b2d850db0288332f59a15ed42c06
Dan Lightner <DanLightner6@gmail.com>
2025-05-10 19:37:42 -0400

    started working on the chatbox feature

---
+++ frontend/src/components/ChatBox.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import "./ChatBox.css";
+
+function ChatBox() {
+    return (
+        <div className="chatbox">
+            
+        </div>
+    )
+}
+export default ChatBox;
